2b6cbaf48aca39857d0a01cb99131f3b714e0c74
Electron7-7 <26826460+Electron7-7@users.noreply.github.com>
2026-02-23 22:27:30 -0500

    build(CMake): initial build system

---
+++ Sources.cmake
@@ -0,0 +1,16 @@
+project(RoseTinted)
+
+set(ROSE_TINTED_SOURCES
+    src/system/dedicated_main.cpp
+    src/gui/imgui_implementor.cpp
+    src/app/rose_tinted.cpp
+    src/things/player.cpp
+    src/thirdparty/DearImGui/imgui.cpp
+    src/thirdparty/DearImGui/imgui_demo.cpp
+    src/thirdparty/DearImGui/imgui_draw.cpp
+    src/thirdparty/DearImGui/imgui_freetype.cpp
+    src/thirdparty/DearImGui/imgui_impl_glfw.cpp
+    src/thirdparty/DearImGui/imgui_impl_opengl3.cpp
+    src/thirdparty/DearImGui/imgui_stdlib.cpp
+    src/thirdparty/DearImGui/imgui_tables.cpp
+    src/thirdparty/DearImGui/imgui_widgets.cpp)
